694d1fd3a2a86c3a49cec3914ac3d5d17837a72b
Prarthna Trivedi <prarthna.trivedi@avantika.edu.in>
2026-03-20 08:11:14 +0000

    version-1

---
+++ identity_prarthna.py
@@ -0,0 +1,23 @@
+# Integer
+a = 20
+print("Integer before change:", id(a))
+a = a + 5
+print("Integer after change:", id(a))
+
+# String
+s = "hello"
+print("\nString before change:", id(s))
+s = s + " world"
+print("String after change:", id(s))
+
+# List
+lst = [6,7,8]
+print("\nList before change:", id(lst))
+lst.append(4)
+print("List after change:", id(lst))
+
+# Tuple
+t = (5,6,7)
+print("\nTuple before change:", id(t))
+t = t + (4,)
+print("Tuple after change:", id(t))
